2773e1a523b4058141c3d8f4ab91f871a6c23a20
Test User <testuser@genes-mbp.lan>
2021-03-10 19:19:41 -0500

    added state in Login, Logout, Register, and UseBar

--- chapter03/src/App.js
+++ chapter03/src/App.js
@@ -1,24 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import PostList from './post/PostList'
 import CreatePost from './post/CreatePost'
 import UserBar from './user/UserBar'
 
-const user = 'Daniel Bugl'
-const posts = [
+const defaultPosts = [
     { title: 'React Hooks', content: 'The greatest thing since sliced bread!', author: 'Daniel Bugl' },
     { title: 'Using React Fragments', content: 'Keeping the DOM tree clean!', author: 'Daniel Bugl' }
 ]
 
 export default function App () {
+  const [ user, setUser ] = useState('')
+  const [posts, setPosts ] = useState(defaultPosts)
+
   return (
-      <div style={{ padding: 8 }}>
-        <UserBar />
+    <div style={{ padding: 8 }}>
+        <UserBar user={user} setUser={setUser} />
         <br />
-        <CreatePost user={user} />
+        {user && <CreatePost user={user} posts={posts} setPosts={setPosts} />}
         <br />
         <hr />
         <PostList posts={posts} />
-        </div>
-    )
+    </div>
+  )
 }
--- chapter03/src/user/UserBar.js
+++ chapter03/src/user/UserBar.js
@@ -4,17 +4,15 @@ import Login from './Login'
 import Logout from './Logout'
 import Register from './Register'
 
-export default function UserBar () {
-  const user = ''
+export default function UserBar ({ user, setUser }) {
 
   if (user) {
-    return <Logout user={user} />
-
+    return <Logout user={user} setUser={setUser} />
   } else {
     return (
         <React.Fragment>
-            <Login />
-            <Register />
+            <Login setUser={setUser} />
+            <Register setUser={setUser} />
         </React.Fragment>
     )
   } 
