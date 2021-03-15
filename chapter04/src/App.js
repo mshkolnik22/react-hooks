f16a1c6c17eb0926f925cd44274f79ea284e032a
Test User <testuser@genes-mbp.lan>
2021-03-15 19:39:34 -0400

    implement and Effect Hook

--- chapter04/src/App.js
+++ chapter04/src/App.js
@@ -1,4 +1,4 @@
-import React, { useReducer } from 'react'
+import React, { useReducer, useEffect } from 'react'
 
 import PostList from './post/PostList'
 import CreatePost from './post/CreatePost'
@@ -10,12 +10,21 @@ const defaultPosts = [
     { title: 'Using React Fragments', content: 'Keeping the DOM tree clean!', author: 'Daniel Bugl' }
 ]
 
-export default function App () {
+export default function App ({ title }) {
     const [ state, dispatch ] = useReducer(appReducer, { user: '', posts: defaultPosts })
     const { user, posts } = state
 
+    useEffect(() => {
+      if (user) {            
+        document.title = `${user} - React Hooks Blog`
+      } else {
+        document.title = 'React Hooks Blog'
+    }
+  }, [user])
+  
     return (
         <div style={{ padding: 8 }}>
+          
             <UserBar user={user} dispatch={dispatch} />
             <br />
             {user && <CreatePost user={user} posts={posts} dispatch={dispatch} />}
