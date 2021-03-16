23a47c90bba87fabf7482e85cd3d6cf88eb72749
Test User <testuser@genes-mbp.lan>
2021-03-16 17:48:20 -0400

    implementing the ChangeTheme component

--- chapter05/src/App.js
+++ chapter05/src/App.js
@@ -1,16 +1,48 @@
-import React from 'react';
-import './App.css';
+import React, { useReducer, useEffect, useState } from 'react'
+
+import PostList from './post/PostList'
+import CreatePost from './post/CreatePost'
+import UserBar from './user/UserBar'
 import Header from './Header'
+import { ThemeContext } from './contexts'
+import ChangeTheme from './ChangeTheme'
+import appReducer from './reducers'
 
-export const ThemeContext = React.createContext({ primaryColor: 'deepskyblue' })
+const defaultPosts = [
+    { title: 'React Hooks', content: 'The greatest thing since sliced bread!', author: 'Daniel Bugl' },
+    { title: 'Using React Fragments', content: 'Keeping the DOM tree clean!', author: 'Daniel Bugl' }
+]
 
-const App = () => (
-  <ThemeContext.Provider value={{ primaryColor: 'coral' }}>
-    <Header text="Hello World" />
-    <ThemeContext.Provider value={{ primaryColor: 'deepskyblue' }}>
-      <Header text="This is a test" />
-    </ThemeContext.Provider>
-</ThemeContext.Provider>
-)
+export default function App () {
+    const [ theme, setTheme ] = useState({
+        primaryColor: 'deepskyblue',
+        secondaryColor: 'coral'
+    })
 
-export default App
+    const [ state, dispatch ] = useReducer(appReducer, { user: '', posts: defaultPosts })
+    const { user, posts } = state
+    
+    useEffect(() => {
+        if (user) {
+            document.title = `${user} - React Hooks Blog`
+        } else {
+            document.title = 'React Hooks Blog'
+        }
+    }, [user])
+
+    return (
+        <ThemeContext.Provider value={theme}>
+            <div style={{ padding: 8 }}>
+                <Header text="React Hooks Blog" />
+                <ChangeTheme theme={theme} setTheme={setTheme} />
+                <br />
+                <UserBar user={user} dispatch={dispatch} />
+                <br />
+                {user && <CreatePost user={user} posts={posts} dispatch={dispatch} />}
+                <br />
+                <hr />
+                <PostList posts={posts} />
+            </div>
+        </ThemeContext.Provider>
+    )
+}
--- chapter05/src/Header.js
+++ chapter05/src/Header.js
@@ -1,10 +1,10 @@
 import React, { useContext } from 'react'
-import { ThemeContext } from './App'
+import { ThemeContext } from './contexts'
 
 const Header = ({ text }) => {
-  const theme = useContext(ThemeContext)
+  const { secondaryColor } = useContext(ThemeContext)
 
-  return <h1 style={{ color: theme.primaryColor }}>{text}</h1>
+  return <h1 style={{ color: secondaryColor }}>{text}</h1>
 }
 
 export default Header
